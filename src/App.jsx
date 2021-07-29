cc35968a34e4310b0f532fa3478ae2bcb6c25350
yosantiagogarcia <santiago.garcia@yotepresto.com>
2021-07-29 12:24:19 -0500

    Pruebas realizadas en el taller.

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,52 @@
-import React, { useState } from 'react';
-import TodoItem from './components/TodoItem';
-import './styles/App.css';
+import React, { useState } from "react";
+import TodoItem from "./components/TodoItem";
+import "./styles/App.css";
 
 function App() {
-    const [input, setInput] = useState('');
-    const [items, setItems] = useState([]);
+  const [input, setInput] = useState("");
+  const [items, setItems] = useState([]);
 
-    function addItem(event) {
-        setItems(prevData => {
-            return [...prevData, input];
-        });
-        setInput('');
-    }
+  function addItem(event) {
+    setItems((prevData) => [...prevData, input]);
+    setInput("");
+  }
 
-    function removeItem(id) {
-        setItems(prevData => {
-            return prevData.filter((item, index) => {
-                return index !== id;
-            })
-        });
-    }
+  function removeItem(id) {
+    setItems((prevData) => prevData.filter((_, index) => index !== id));
+  }
 
-    return (
-      <div className="todolist">
-          <div className="heading">
-              <h1 className="title">YTP To-Do</h1>
-          </div>
-              <input
-                  type="text"
-                  value={input}
-                  onChange={(event) => {setInput(event.target.value)}}
-              />
-              <button onClick={addItem}>Add</button>
-
-          <div className="items">
-            <ul>
-                {items.map((item, index) => (
-                    <TodoItem
-                        key={index}
-                        id={index}
-                        item={item}
-                        onCheck={removeItem}
-                    />
-                ))}
-            </ul>
-          </div>
+  return (
+    <div className="todolist">
+      <div className="heading">
+        <h1 className="title">YTP To-Do</h1>
       </div>
-    );
+      <input
+        type="text"
+        value={input}
+        aria-label="todo-input"
+        onChange={(event) => {
+          setInput(event.target.value);
+        }}
+      />
+      <button onClick={addItem} aria-label="add-button">
+        +
+      </button>
+
+      <div className="items">
+        <ul>
+          {items.map((item, index) => (
+            <TodoItem
+              key={index}
+              id={index}
+              data-testid={`item-${index}`}
+              item={item}
+              onCheck={removeItem}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,9 +1,44 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
+import { render, fireEvent } from "@testing-library/react";
+import App from "../App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/YTP To-Do/i);
-  
-  expect(linkElement).toBeInTheDocument();
+describe("TODO", () => {
+  it("renders the title", () => {
+    const expectedTitle = "YTP To-Do";
+    const { getByText } = render(<App />);
+    expect(getByText(expectedTitle)).toBeInTheDocument();
+  });
+
+  it("writes input", () => {
+    const expectedInputText = "Hello mom! Im using jest";
+    const { getByLabelText, getByText } = render(<App />);
+
+    const input = getByLabelText("todo-input");
+
+    // fireEvent.change(input, {target: {value: '$23.0'}})
+    fireEvent.change(input, {
+      target: {
+        value: expectedInputText
+      }
+    });
+
+    expect(input).toHaveAttribute("value", expectedInputText);
+
+    // Click en el botón de agregar
+    const button = getByLabelText("add-button");
+
+    fireEvent.click(button);
+
+    // se muestre en el todo-list
+    expect(getByText(expectedInputText)).toBeInTheDocument();
+
+    // se borre el input
+    expect(input.value).toBe("");
+
+    // eliminar el registro
+    const todo = getByText(expectedInputText);
+    fireEvent.click(todo);
+
+    expect(todo).not.toBeInTheDocument();
+    // expect(getAllByTestId("item-0")).toBeInTheDocument();
+  });
 });
